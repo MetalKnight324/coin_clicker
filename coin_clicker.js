de3c62bfc5e2665073f8cb66fb65a81788b01dc0
Eli Knight <eli.knight324@gmail.com>
2016-08-07 16:11:50 -0500

    start oo implementation
    reference jQuery and underscore.js
    create Business class
    remove business variables before oo

--- coin_clicker.js
+++ coin_clicker.js
@@ -1,17 +1,69 @@
+function Business(description, priceSchedule) {
+  this.description = description;
+  this.priceSchedule = priceSchedule;
+  this.currentLevel = -1;
+}
+Business.prototype = {
+  getDescription: function() {
+    return this.description;
+  },
+  getCoinRate: function() {
+    if(this.currentLevel == -1) {
+      return 0;
+    } else {
+      return this.priceSchedule[this.currentLevel].coinRate;
+    }
+  },
+  getPurchaseCost: function() {
+    if(this.currentLevel == (this.priceSchedule.length - 1)) {
+      return 0;
+    }
+    return this.priceSchedule[this.currentLevel + 1].cost;
+  },
+  purchase: function() {
+    if(this.currentLevel < (this.priceSchedule.length - 1)) {
+      this.currentLevel += 1;
+    } else {
+      console.log("attempt to purchase a level that is not available");
+    }
+  }
+}
+
+var businesses = [
+  new Business("Lemonade Stand", [
+    {
+      cost: 100,
+      coinRate: 10
+    },
+    {
+      cost: 1500,
+      coinRate: 30
+    }
+  ]),
+  new Business("Pizzaria", [
+    {
+      cost: 500,
+      coinRate: 65
+    },
+    {
+      cost: 7000,
+      coinRate: 195
+    }
+  ]),
+  new Business("Donut Shop", [
+    {
+      cost: 2000,
+      coinRate: 300
+    },
+    {
+      cost: 20000,
+      coinRate: 1000
+    }
+  ])
+];
 var coinCount = 1600;
 var clickRate = 1;
 var doubleClickRateCost = 10;
-var perSecondRate = 0;
-var lemonadeCost = 100;
-var pizzariaCost = 500;
-var donutCost = 2000;
-var arcadeCost = 10000;
-var lemonadePS = 10;
-var pizzariaPS = 65;
-var donutPS = 300;
-var arcadePS = 1750;
-var lemonadeStands = 0;
-var lemonadePScost = 1500;
 
 function drawCircle() {
   var canvas = document.getElementById("game-board");
@@ -23,12 +75,15 @@ function drawCircle() {
 }
 
 function afterCoinCountChange() {
-  enablePurchaseButton("clickRateButton", doubleClickRateCost);
-  enablePurchaseButton("lemonadeButton", lemonadeCost);
-  enablePurchaseButton("pizzariaButton", pizzariaCost);
-  enablePurchaseButton("donutButton", donutCost);
-  enablePurchaseButton("arcadeButton", arcadeCost);
-  enablePurchaseButton("lemonadeUpButton", lemonadePScost);
+   $('#clickRateButton').prop('disabled', coinCount < doubleClickRateCost);
+   _.each(businesses, function(business, index) {
+     $('#business-' + index + '-button').prop('disabled', coinCount < business.getPurchaseCost());
+   });
+  // enablePurchaseButton("lemonadeButton", lemonadeCost);
+  // enablePurchaseButton("pizzariaButton", pizzariaCost);
+  // enablePurchaseButton("donutButton", donutCost);
+  // enablePurchaseButton("arcadeButton", arcadeCost);
+  // enablePurchaseButton("lemonadeUpButton", lemonadePScost);
   showStats();
 }
 
@@ -44,71 +99,55 @@ function purchaseClickRate() {
   afterCoinCountChange();
 }
 
-function purchaseLemonade() {
-  perSecondRate += lemonadePS;
-  coinCount = coinCount - lemonadeCost;
-  lemonadeCost = Math.round(lemonadeCost * 2.5)
-  afterCoinCountChange();
-}
-
-function purchasePizzaria() {
-  perSecondRate += pizzariaPS;
-  coinCount = coinCount - pizzariaCost;
-  pizzariaCost = Math.round(pizzariaCost * 2.5)
-  afterCoinCountChange();
-}
-
-function purchaseDonutShop() {
-  perSecondRate += donutPS;
-  coinCount = coinCount - donutCost;
-  donutCost = Math.round(donutCost * 2.5);
-  afterCoinCountChange();
-}
-
-function purchaseArcade() {
-   perSecondRate += arcadePS;
-   coinCount = coinCount - arcadeCost;
-   arcadeCost = Math.round(arcadeCost * 2.5);
-   afterCoinCountChange();
- }
-
- function upgradeLemonade() {
-    lemonadePS *= 3;
-    coinCount = coinCount - lemonadePScost;
-    lemonadePScost = Math.round(lemonadePScost * 5);
-    afterCoinCountChange();
-  }
-
-
-function enablePurchaseButton(buttonId, minimumCoinCount) {
-  var button = document.getElementById(buttonId);
-  if(coinCount >= minimumCoinCount) {
-    button.disabled = false;
-  } else {
-    button.disabled = true;
-  }
-}
-
 function addPerSecond() {
-  console.log("Coins Per Second");
-  coinCount = coinCount + perSecondRate;
+  var businessCoins = _.reduce(businesses, function(total, business) {
+    return total + business.getCoinRate();
+  }, 0);
+  coinCount += businessCoins;
   afterCoinCountChange();
 }
 
 function showStats() {
   document.getElementById("coinsCell").innerHTML = coinCount;
   document.getElementById("clickRateCell").innerHTML = clickRate;
-  document.getElementById("perSecondCell").innerHTML = perSecondRate;
+//  document.getElementById("perSecondCell").innerHTML = perSecondRate;
   document.getElementById("doubleClickRateCell").innerHTML = doubleClickRateCost;
-  document.getElementById("lemonadeCostCell").innerHTML = lemonadeCost;
-  document.getElementById("pizzariaCostCell").innerHTML = pizzariaCost;
-  document.getElementById("donutCostCell").innerHTML = donutCost;
-  document.getElementById("arcadeCostCell").innerHTML = arcadeCost;
-  document.getElementById("lemonadeUpCell").innerHTML = lemonadePScost;
+  // document.getElementById("lemonadeCostCell").innerHTML = lemonadeCost;
+  // document.getElementById("pizzariaCostCell").innerHTML = pizzariaCost;
+  // document.getElementById("donutCostCell").innerHTML = donutCost;
+  // document.getElementById("arcadeCostCell").innerHTML = arcadeCost;
+  // document.getElementById("lemonadeUpCell").innerHTML = lemonadePScost;
+}
+
+function showBusinesses() {
+   _.each(businesses, function(business, index) {
+     var lastRow = $('#gameTable div.row:last');
+     console.log(lastRow[0].className);
+     lastRow.after(
+       '<div class="row">' +
+         '<div class="col-sm-6">' +
+            '<h2>' + business.description + '</h2>' +
+         '</div>' +
+         '<div class="col-sm-6">' +
+           '<span id="business-' + index + '-cost"></span>' +
+         '</div>' +
+        '</div>' +
+        '<div class="row">' +
+          '<div class="col-sm-12">' +
+            '<button id="business-' + index + '-button" class="btn btn-default">Purchase</button>' +
+          '</div>' +
+       '</div>');
+    $('#business-' + index + '-button').click(function() {
+      coinCount -= business.getPurchaseCost();
+      business.purchase();
+      afterCoinCountChange();
+    });
+   })
 }
 
 function init() {
   drawCircle();
-  afterCoinCountChange();
+//  afterCoinCountChange();
   setInterval(addPerSecond, 1000);
+  showBusinesses();
 }
